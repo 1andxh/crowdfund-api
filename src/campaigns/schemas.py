d5c7dd83579777cf2dfdb44b835ed647e6d1cc8a
andoh <solomonkingsman@gmail.com>
2026-02-20 03:23:26 +0000

    Add campaign schemas

---
+++ src/campaigns/schemas.py
@@ -0,0 +1,57 @@
+from pydantic import BaseModel, Field, ConfigDict, field_validator
+from datetime import datetime, timezone
+import uuid
+from .models import CampaignStatus
+
+now = datetime.now(timezone.utc)
+
+
+class CampaignCreate(BaseModel):
+    title: str = Field(min_length=5, max_length=200)
+    description: str = Field(min_length=10, max_length=500)
+    story: str | None = None
+    goal_amount: float = Field(gt=0, description="Goal amount in GHS")
+    deadline: datetime
+    image_url: str | None = None
+
+    @field_validator("deadline")
+    @classmethod
+    def deadline_validator(cls, v: datetime) -> datetime:
+        if v.tzinfo is None:
+            v = v.replace(tzinfo=timezone.utc)
+
+        if v <= now:
+            raise ValueError("Deadline must be in the future")
+        return v
+
+
+class CampaignResponse(BaseModel):
+    id: uuid.UUID
+    creator_id: uuid.UUID
+    title: str
+    description: str
+    story: str | None
+    goal_amount: float
+    current_amount: float
+    image_url: str | None
+    deadline: datetime
+    status: CampaignStatus
+    created_at: datetime
+    updated_at: datetime | None
+    ended_at: datetime | None
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class CampaignList(BaseModel):
+    id: uuid.UUID
+    creator_id: uuid.UUID
+    title: str
+    description: str
+    goal_amount: float
+    current_amount: float
+    image_url: str | None
+    deadline: datetime
+    status: CampaignStatus
+
+    model_config = ConfigDict(from_attributes=True)
